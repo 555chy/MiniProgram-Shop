b4d892985369fddb4a90750b282460ba4fca2b8c
钢铁加鲁鲁 <chenhy@fastlion.cn>
2020-05-29 10:52:53 +0800

    重置密码倒计时

--- miniprogram/pages/forget/forget.js
+++ miniprogram/pages/forget/forget.js
@@ -6,28 +6,32 @@ Page({
   /**
    * 页面的初始数据
    */
-  data: {
+  data: {    
     phone: '',
-    smsCode: ''
+    smsCode: '',
+    time: "获取验证码",
+    totalTime: 3,
+    currentTime: 0,
+    disabled: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      currentTime: this.data.totalTime
+    })
   },
 
-  getPhone: function(e){
+  getPhone: function (e) {
     let phone = e.detail.value
     this.setData({
       phone
     })
   },
 
-  verify: function(e){
-    let phone = this.data.phone
-
+  verify: function (e) {
     Bmob.requestSmsCode(params).then(res => {
       console.log(res)
       that.setData({
@@ -37,8 +41,28 @@ Page({
     }).catch(err => {
       console.log(err)
     })
-    
-    
-  }
+  },
+  getCode: function (options) {
+    var that = this;
+    var currentTime = that.data.currentTime;
+    that.setData({
+      time: currentTime + '秒',
+      disabled: true
+    })
+    var interval = setInterval(function () {
+      that.setData({
+        time: (currentTime - 1) + '秒'
+      })
+      currentTime--;
+      if (currentTime <= 0) {
+        clearInterval(interval)
+        that.setData({
+          time: '重新获取',
+          currentTime: that.data.totalTime,
+          disabled: false
+        })
+      }
+    }, 1000)
+  },
 
 })
