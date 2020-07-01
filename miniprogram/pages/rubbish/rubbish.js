97b985648dea379af2818fe6b52e8aaeaf4eab0e
钢铁加鲁鲁 <chenhy@fastlion.cn>
2020-07-01 20:57:21 +0800

    侧边栏分类

--- miniprogram/pages/rubbish/rubbish.js
+++ miniprogram/pages/rubbish/rubbish.js
@@ -1,19 +1,29 @@
 
 Page({
   data: {
-    rubbishs: [],
+    curNav: 0,
+    title: [],
+    content: [],
     order: 0
   },
 
   onLoad: function (options) {
     const eventChannel = this.getOpenerEventChannel()
     eventChannel.on('rubbish', data => {
-      console.log(data.rubbish)
+      console.log(data.rubbish.rubbishs)
       this.setData({
-        rubbishs: data.rubbish.rubbishs,
-        order: data.rubbish.order
-      })
+        title: data.rubbish.rubbishs,
+        curNav: 0,
+        content: data.rubbish.rubbishs[0].array
+      });
+      console.log(this.data)
+    })
+  },
+
+  switchRightTab: function(e) {
+    this.setData({
+      curNav: e.target.dataset.index,
+      content: this.data.title[this.data.curNav].array
     })
   }
-
 })
