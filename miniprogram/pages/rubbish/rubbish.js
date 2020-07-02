875ad0719efe09f7fa863f3954763e16e4b28261
黄欣凯 <huangxk@fastlion.cn>
2020-07-02 18:35:05 +0800

    sad

--- miniprogram/pages/rubbish/rubbish.js
+++ miniprogram/pages/rubbish/rubbish.js
@@ -4,26 +4,31 @@ Page({
     curNav: 0,
     title: [],
     content: [],
+    rubbishs:[],
     order: 0
   },
 
   onLoad: function (options) {
     const eventChannel = this.getOpenerEventChannel()
     eventChannel.on('rubbish', data => {
-      console.log(data.rubbish.rubbishs)
+      console.log(data.rubbish)
       this.setData({
+        order: data.rubbish.order,
         title: data.rubbish.rubbishs,
+        rubbishs: data.rubbish.rubbishs,
         curNav: 0,
         content: data.rubbish.rubbishs[0].array
       });
+      console.log(this.data.curNav)
       console.log(this.data)
     })
   },
 
   switchRightTab: function(e) {
+    const index = e.target.dataset.index;
     this.setData({
-      curNav: e.target.dataset.index,
-      content: this.data.title[this.data.curNav].array
+      curNav: index,
+      content: this.data.title[index].array
     })
   }
 })
